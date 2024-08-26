8a2c47d9d772d97c4f0d9586b9288138a2f147f0
Bluedigi <edmvp98@gmail.com>
2024-08-26 14:13:31 -0600

    first commit

---
+++ src/test/java/com/bluered/pokemon/PokemonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluered.pokemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
